fcaff8d1f6887090e2f26f1ac561420b775f7952
Alex Gaudio <adgaudio@gmail.com>
2011-07-01 09:11:26 -0400

    added docs

--- sql/q3choices.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO talks_q3choices VALUES (0,'beginner');
-INSERT INTO talks_q3choices VALUES (1,'intermediate');
-INSERT INTO talks_q3choices VALUES (2,'advanced');
--- sql/talkstate.sql
+++
@@ -1,6 +0,0 @@
-
-INSERT INTO talks_talkstate  VALUES (1,'Not yet accepting talks');
-INSERT INTO talks_talkstate  VALUES (2,'Accepting talks');
-INSERT INTO talks_talkstate  VALUES (3,'Talks are locked and no additions or edits allowed');
-INSERT INTO talks_talkstate  VALUES (4,'Talk voting is on');
-INSERT INTO talks_talkstate  VALUES (5,'Talk voting is complete');
