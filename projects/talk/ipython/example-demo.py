08beeea297b98a6a7e3686f55f08ef20dc4497d4
Alex Gaudio <adgaudio@gmail.com>
2011-09-14 11:00:05 -0400

    talk reorganizing pt1

---
+++ projects/talk/ipython/example-demo.py
@@ -0,0 +1,1 @@
+/opt/local/Library/Frameworks/Python.framework/Versions/2.7/share/doc/ipython/examples/lib/example-demo.py
---
+++ projects/talk/old/embed_ipython.py
@@ -0,0 +1,11 @@
+a = [1,2]
+
+
+from IPython.Shell import IPShellEmbed
+
+ipshell = IPShellEmbed()
+print
+print 'embed_ipython: This is my interactive IPython shell'
+ipshell()
+print 'embed_ipython: Closing interactive shell'
+print
