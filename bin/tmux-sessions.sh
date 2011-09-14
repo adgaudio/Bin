90a4d8be42757c61d5a063ae507ae148a56f5621
Alex Gaudio <adgaudio@gmail.com>
2011-09-14 11:01:18 -0400

    tmux-sessions: cleaned up

--- bin/tmux-sessions.sh
+++ bin/tmux-sessions.sh
@@ -3,9 +3,20 @@
 #Register sessions that will be created by this script
 SESSIONS="fwk hi daemons"
 
+#
+#Define logic
+#
+function has-session {
+    tmux has-session -t $1 2>/dev/null
+}
+
+function list-sessions {
+    echo `tmux list-sessions | cut -d: -f1 | sort -r`
+}
+
 function try-attach 
 {
-    tmux has-session -t $1 2>/dev/null
+    has-session $1
     rc="$?"
     attached="`tmux list-clients -t $1 2>/dev/null`"
     
@@ -16,13 +27,6 @@ function try-attach
     fi
 }
 
-function has-session {
-    tmux has-session -t $1 2>/dev/null
-}
-
-function list-sessions {
-    echo `tmux list-sessions | cut -d: -f1 | sort -r`
-    }
 function except 
 {
     if [ "$?" -eq 1 ] ; then
@@ -30,6 +34,9 @@ function except
     fi
 }
 
+#
+# Session Configuration
+#
 function session-fwk 
 {
     tmux new-session -d -s fwk
@@ -54,6 +61,9 @@ function session-hi
     tmux neww -k -t hi:1
 }
 
+#
+# Implementation
+#
 
 # Create sessions if they don't exist
 for x in $SESSIONS
@@ -61,7 +71,6 @@ do
     has-session $x
     except session-$x
 done
-
 
 tmux link-window -dk -s daemons:manage -t hi:0 2>/dev/null
 
@@ -71,7 +80,8 @@ for x in $(list-sessions) ; do
     echo "Couldn't attach to session $x"
 done
 
-# if all sessions already attached to, create a new one and attach
+# if all sessions already attached to,
+# create a new one and attach
 tmux new
 
 
