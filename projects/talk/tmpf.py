098eff9a3a852be712b3dd32f37619ac889b7d18
Alex Gaudio <adgaudio@gmail.com>
2011-09-16 00:18:25 -0400

    unnec file

--- projects/talk/tmpf.py
+++
@@ -1,9 +0,0 @@
-a = 1
-
-from IPython import embed
-embed() # drop into an IPython session.
-        # Any variables you define or modify here
-        # will not affect program execution
-
-c = 2
-
