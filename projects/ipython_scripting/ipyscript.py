71ad18c0bb3102a423da6955ca69902aaf64fe62
Alex Gaudio <adgaudio@gmail.com>
2011-10-07 08:43:46 -0400

    ipyscript updates. way better

--- projects/ipython_scripting/ipyscript.py
+++ projects/ipython_scripting/ipyscript.py
@@ -10,7 +10,7 @@ and things like accessing output history, directory history, etc"""
 from IPython.frontend.terminal.interactiveshell import TerminalInteractiveShell
 
 
-def execute(file_, verbose=0):
+def execute(file_, verbose = 0, stop_on_error = 1, store_history = 0):
     """ Read and execute lines of code in given open file.
     Supports pure python or ipython-bash style syntax,
     multi-line code, and the IPython feature set
@@ -20,30 +20,34 @@ def execute(file_, verbose=0):
     c = shell.get_ipython()
     c.instance() # initialize ipython config
 
-    raw_cell = file_.readlines()
+    raw_cells = file_.readlines()
     exception = None
 
-    while raw_cell:
+    while raw_cells:
         # Extract smallest possible executable block of code from raw source
-        is_completed = c.input_splitter.push(raw_cell.pop(0))
+        is_completed = c.input_splitter.push(raw_cells.pop(0))
         while not is_completed:
-            is_completed = c.input_splitter.push(raw_cell.pop(0))
+            is_completed = c.input_splitter.push(raw_cells.pop(0))
 
-        try:
-            cell = c.input_splitter.source_reset()
-            # Transform cell into syntactically correct python
-            cell = c.prefilter_manager.prefilter_lines(cell) + '\n'
-            # Compile to byte code
-            code = compile(cell, 'cellname', 'exec')
+        cell, raw_cell = c.input_splitter.source_raw_reset()
+        # Transform cell into syntactically correct python
+        cell = c.prefilter_manager.prefilter_lines(cell) + '\n'
 
-            if verbose:
-                print '========'
-                print 'executing:', cell
-                print '========'
-            c.run_code(code)
-        except Exception, e:
-            raise
-            import ipdb ; ipdb.set_trace()
+        # If we are storing script in/out history
+        if store_history:
+            c.history_manager.store_inputs(c.execution_count,
+                                              cell, raw_cell)
+        # Compile to byte code
+        code = compile(cell, 'cellname', 'exec')
+        if verbose:
+            print '========'
+            print 'executing:', cell
+            print '========'
+        outflag = c.run_code(code)
+
+        if stop_on_error and outflag:
+            c.exit()
+            break
 
 if __name__ == '__main__':
     import sys
