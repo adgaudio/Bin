aeecf9d973b77c534bd644a107063b6f1520a621
Alex Gaudio <adgaudio@gmail.com>
2011-12-20 00:14:01 -0500

    tmux optimized for linux (not osx)

--- projects/tmux/tmux-sessions.sh
+++ projects/tmux/tmux-sessions.sh
@@ -1,24 +1,28 @@
 # Automatically create a new session with these windows
 
 #Register sessions that will be created by this script
-SESSIONS="fwk hi daemons"
-
+SESSIONS="hi daemons"
 #
 #Define logic
 #
 function has-session {
-    tmux has-session -t $1 2>/dev/null
+    tmux has-session -t $1
+    rc="$?"
+    if [ "$rc" -eq "0" ] ; then
+        return 0
+    fi
+    return 1
 }
 
 function list-sessions {
-    echo `tmux list-sessions | cut -d: -f1 | sort -r`
+    echo $( tmux list-sessions | cut -d: -f1 | sort -r )
 }
 
 function try-attach 
 {
     has-session $1
     rc="$?"
-    attached="`tmux list-clients -t $1 2>/dev/null`"
+    attached="$( tmux list-clients -t $1 )"
     
     if [ "$rc" -eq "0" ] && [ ! "$attached" ]; then
         (tmux attach -t $1)
@@ -42,11 +46,9 @@ function session-fwk
     tmux new-session -d -s fwk
     tmux neww -k -n sreports -t fwk:1
     tmux neww -k -n prod -t fwk:2 
-    tmux neww -k  -t fwk:3 
-    tmux neww -k -n local -t fwk:4 
     
-    tmux send-keys -t fwk:1 'sreports'
-    tmux send-keys -t fwk:2 'reports'
+    tmux send-keys -t fwk:1 ''
+    tmux send-keys -t fwk:2 ''
 }
 
 function session-daemons
@@ -64,7 +66,6 @@ function session-hi
 #
 # Implementation
 #
-
 # Create sessions if they don't exist
 for x in $SESSIONS
 do
@@ -72,7 +73,7 @@ do
     except session-$x
 done
 
-tmux link-window -dk -s daemons:manage -t hi:0 2>/dev/null
+tmux link-window -dk -s daemons:manage -t hi:0
 
 # Attach to any existing session with no attached clients
 for x in $(list-sessions) ; do
@@ -98,7 +99,6 @@ tmux new
 
 function has-sessions {
     for x in $@ ; do
-	echo $x
         tmux has-session -t $x
 	rc=$rc+$?
     return rc
