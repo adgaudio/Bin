1bf48e9238b673c93dfc89c6dbc7e207d2d66bbe
Alex Gaudio <adgaudio@gmail.com>
2015-03-20 04:03:14 -0400

    get lat and long and plot on a map

--- projects/foundationcenter/scraper.py
+++ projects/foundationcenter/scraper.py
@@ -1,8 +1,9 @@
-import requests
-import pandas as pd
-import os
+from functools import wraps, lru_cache
+from geopy.geocoders import Nominatim
 from os.path import exists
-from functools import wraps
+import os
+import pandas as pd
+import requests
 
 
 def c(*args):
@@ -80,9 +81,12 @@ def parse_html_table(resp, pagenum, fp):
     return df
 
 
-# fetch all pages
-# currently between 925 and 930 pages of foundations on the site
 def get_dfs(npages):
+    """
+    fetch all pages
+    currently between 925 and 930 pages of foundations on the site
+    """
+    print("loading data")
     try:
         os.makedirs('./data')
     except FileExistsError:
@@ -101,5 +105,65 @@ def get_dfs(npages):
     df = pd.concat(dfs)
     return df
 
-df = get_dfs(930)  # 930
-df.dropna().plot(kind='scatter', x='Total Assets.log', y='Total Giving.log')
+
+def get_lat_lon(df, fp='./data/city_lat_long.csv'):
+    """
+    get latitude and longitude of each city for given fram
+    """
+    print("get city lat and long")
+    if exists(fp):
+        return pd.read_csv(fp, index_col=0)
+    print("... this may take some time")
+    geolocator = Nominatim()
+    _lat_lon = {}
+    for city in df['City, State/Country'].unique():
+        l = geolocator.geocode(city)
+        if not l:
+            print("... SKIP", city)
+            continue
+        _lat_lon[city] = l[1]
+    lat_lon = pd.DataFrame.from_dict(_lat_lon, orient='index')
+    lat_lon.columns = ['lat', 'lon']
+    lat_lon.index.name = 'City, State/Country'
+    lat_lon.to_csv(fp)
+    return lat_lon
+
+
+def plots(df):
+    import pylab as plt
+    from mpl_toolkits import basemap
+
+    print("plot things")
+
+    plt.ion()
+
+    # total giving vs total assets
+    df.dropna().plot(kind='scatter', x='Total Assets.log', y='Total Giving.log')
+    plt.figure()
+
+    # plot each city on a map
+    usmap = basemap.Basemap(
+        llcrnrlon=-130, llcrnrlat=24, urcrnrlon=-64, urcrnrlat=50)
+    usmap.drawcountries(color='gray', linewidth='.6')
+    usmap.drawcoastlines(color='gray')
+    usmap.drawstates()
+    agg_by_city = get_lat_lon(df).join(
+        df.groupby('City, State/Country')['Total Giving'].agg(
+            {'Total Giving ($)': lambda x: x.sum(),
+             'Total Giving (num grants)': lambda x: x.count()}))
+
+    def get_markersize(x, min, max):
+        l = pd.np.log
+        return (l(x) - l(min)) / (l(max) - l(min)) * (20-5) + 5
+    min, max = agg_by_city.min(), agg_by_city.max()
+    for row in agg_by_city.itertuples():
+        t = 'Total Giving ($)'
+        # t = 'Total Giving (num grants)'
+        usmap.plot(
+            row[2], row[1], 'bo',
+            markersize=get_markersize(row[3], min.ix[t], max.ix[t]))
+
+
+df = get_dfs(927)  # 930
+plots(df)
+input('hit enter...')
